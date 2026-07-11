a9732a04ce784d12cb329f413400967f116f240a
Carlos Roberto Silva <csilva@graniteschools.org>
2026-07-11 01:13:14 -0600

    Collect and display user information
    Added prompts to collect user information including name, mailing address, and phone number, and display the collected data.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Declare variables to hold user information
+        string name;
+        string mailingAddress;
+        string phoneNumber;
+
+        // Prompt for and read input
+        Console.Write("Please enter your name:");
+        name = Console.ReadLine();
+
+        Console.Write("Please enter your mainling Address:");
+        mailingAddress = Console.ReadLine();
+
+        Console.Write("Please enter your phone number:");
+        phoneNumber = Console.ReadLine();
+
+        // Display the collected information
+        Console.WriteLine();
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Mailing Address: {mailingAddress}");
+        Console.WriteLine($"Phone Number: {phoneNumber}");
     }
 }
