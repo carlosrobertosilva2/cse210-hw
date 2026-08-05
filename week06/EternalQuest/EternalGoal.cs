decd379ad25d5c7ee8351f8f4ecb12dca483c3cc
Carlos Roberto Silva <csilva@graniteschools.org>
2026-08-05 08:28:45 -0600

    Change RecordEvent to return points instead of void
    EternalGoal Change

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -5,9 +5,9 @@ public class EternalGoal : Goal
     {
     }
 
-    public override void RecordEvent()
+    public override int RecordEvent()
     {
-        // Eternal goals never become complete.
+        return GetPoints();
     }
 
     public override bool IsComplete()
