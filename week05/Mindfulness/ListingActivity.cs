7710ce00967d3d202739c65f6f3ce306763a7676
Carlos Roberto Silva <csilva@graniteschools.org>
2026-08-06 00:06:59 -0600

    Refactor ListingActivity to simplify prompt handling
    Refactor ListingActivity to simplify prompt handling

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,45 +1,39 @@
-using System;
-using System.Collections.Generic;
-
 public class ListingActivity : Activity
 {
-    private readonly List<string> _prompts;
-    private readonly Random _random;
+    private readonly List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
 
     public ListingActivity()
         : base(
             "Listing Activity",
             "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
     {
-        _random = new Random();
-
-        _prompts = new List<string>
-        {
-            "Who are people that you appreciate?",
-            "What are personal strengths of yours?",
-            "Who are people that you have helped this week?",
-            "When have you felt the Holy Ghost this month?",
-            "Who are some of your personal heroes?"
-        };
     }
 
-    public void Run()
+    public override void Run()
     {
         DisplayStartingMessage();
 
+        string prompt = Shuffle(_prompts)[0];
         Console.WriteLine("List as many responses as you can to the following prompt:\n");
-        Console.WriteLine($"--- {_prompts[_random.Next(_prompts.Count)]} ---\n");
-        Console.Write("You may begin in: ");
+        Console.WriteLine($" --- {prompt} --- ");
+        Console.Write("\nYou may begin in: ");
         ShowCountdown(5);
-        Console.WriteLine();
+        Console.WriteLine("\n");
 
-        int count = 0;
         DateTime endTime = DateTime.Now.AddSeconds(GetDuration());
+        int count = 0;
 
         while (DateTime.Now < endTime)
         {
             Console.Write("> ");
-            string? item = Console.ReadLine();
+            string item = Console.ReadLine() ?? "";
 
             if (!string.IsNullOrWhiteSpace(item))
             {
@@ -47,7 +41,9 @@ public class ListingActivity : Activity
             }
         }
 
-        Console.WriteLine($"You listed {count} item{(count == 1 ? "" : "s")}!");
+        Console.WriteLine($"\nYou listed {count} item{(count == 1 ? "" : "s")}!");
         DisplayEndingMessage();
     }
+
 }
+
