575f46f622d59a1e670c421be8c5405998ea9b51
Carlos Roberto Silva <csilva@graniteschools.org>
2026-08-06 00:52:37 -0600

    Refactor Program.cs to simplify user interaction
    Refactor Program.cs to simplify user interaction

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,87 +1,43 @@
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers = CollectNumbers();
+        DisplayWelcome();
 
-        if (numbers.Count == 0)
-        {
-            Console.WriteLine("No numbers were entered.");
-            return;
-        }
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
 
-        Console.WriteLine($"\nSum: {ComputeSum(numbers)}");
-        Console.WriteLine($"Average: {ComputeAverage(numbers):F2}");
-        Console.WriteLine($"Largest: {FindLargest(numbers)}");
-        Console.WriteLine($"Smallest above average: {FindSmallestAboveAverage(numbers)}");
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(userName, squaredNumber);
     }
 
-    // Prompts the user to enter numbers until 0 is entered
-    static List<int> CollectNumbers()
+    static void DisplayWelcome()
     {
-        Console.WriteLine("Enter numbers one at a time. Type 0 when you are done.");
-        List<int> numbers = new List<int>();
-
-        while (true)
-        {
-            Console.Write("Enter number: ");
-            int number = int.Parse(Console.ReadLine());
-
-            if (number == 0)
-            {
-                break;
-            }
-
-            numbers.Add(number);
-        }
-
-        return numbers;
+        Console.WriteLine("Welcome to the Program!");
     }
 
-    static int ComputeSum(List<int> numbers)
+    static string PromptUserName()
     {
-        int sum = 0;
-        foreach (int n in numbers)
-        {
-            sum += n;
-        }
-        return sum;
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine() ?? "";
     }
 
-    static double ComputeAverage(List<int> numbers)
+    static int PromptUserNumber()
     {
-        return (double)ComputeSum(numbers) / numbers.Count;
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine() ?? "0");
     }
 
-    static int FindLargest(List<int> numbers)
+    static int SquareNumber(int number)
     {
-        int largest = numbers[0];
-        foreach (int n in numbers)
-        {
-            if (n > largest)
-            {
-                largest = n;
-            }
-        }
-        return largest;
+        return number * number;
     }
 
-    static int FindSmallestAboveAverage(List<int> numbers)
+    static void DisplayResult(string userName, int squaredNumber)
     {
-        double average = ComputeAverage(numbers);
-        int smallest = int.MaxValue;
-
-        foreach (int n in numbers)
-        {
-            if (n > average && n < smallest)
-            {
-                smallest = n;
-            }
-        }
-
-        return smallest == int.MaxValue ? -1 : smallest;
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
 }
